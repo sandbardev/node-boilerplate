5e19c625e0673912ec3c4c0bf01ab732baaf7581
sandobits <sandersonbarbosa36@gmail.com>
2020-08-07 17:16:22 -0300

    Add dotenv dependency

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require('@babel/register')
-// require('dotenv').config()
+require('dotenv').config()
 
 const app = require('./src/app').default
 
-const environment = 'development'
-const PORT = 3333
+const environment = process.env.NODE_ENV || 'development'
+const port = process.env.PORT
 
 app.listen(
-  PORT, () => console.log(`App listening on port ${3333}`)
+  port, () => console.log(`App listening on port ${3333}, ${environment} enviroment`)
 )
